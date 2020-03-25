aae1efd43f033b9c53e776aa3d1372888af9c63e
Hanzo- <hmickelson@gmail.com>
2020-03-25 16:54:07 -0500

    Update keymap.c

--- firmware/QMKv2/keymaps/default/keymap.c
+++ firmware/QMKv2/keymaps/default/keymap.c
@@ -5,27 +5,30 @@
 void encoder_update_user(uint8_t index, bool clockwise) {
   if (index == 0) { /* First encoder */
     if (clockwise) {
-      tap_code(KC_1);
+      tap_code(KC_EQUAL);
     } else {
-      tap_code(KC_2);
+      tap_code(KC_MINUS);
     }
-  } else if (index == 1) { /* Second encoder */  
+  /*} else if (index == 1)*/ { /* Second encoder */  
+   /* if (clockwise) {*/
+   /*   tap_code(KC_3);*/
+   /* } else {*/
+   /*   tap_code(KC_4);*/
+   /* }*/
+  } else if (index == 5) { /* 5th encoder */  
     if (clockwise) {
-      tap_code(KC_3);
+      tap_code(LGUI(KC_RIGHT));
     } else {
-      tap_code(KC_4);
-    }
-  } else if (index == 2) { /* Third encoder */  
-    if (clockwise) {
-      tap_code(KC_5);
-    } else {
-      tap_code(KC_6);
+      tap_code(LGUI(KC_LEFT));
     }
   }
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { //buttion closest to usb is first
-  [_MAIN] = LAYOUT(
-     KC_A, KC_B, KC_C, KC_D, KC_E 
+  [0] = LAYOUT(
+     TG(1), LGUI(KC_U), KC_I, KC_O, KC_X 
+  )
+  [1] = LAYOUT(
+     TG(0), KC_C, KC_D, KC_BSPACE, KC_X 
   )
 };
